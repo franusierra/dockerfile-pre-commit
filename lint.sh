1e84314f2e48a9ca0df3956e76e120c6a2bd5c7a
franusierra <franusierra@gmail.com>
2020-03-11 14:02:00 +0100

    feat: add echos and permissions on hadolint install on ubuntu

--- lint.sh
+++ lint.sh
@@ -7,19 +7,22 @@ set -o nounset
 export PATH=$PATH:/usr/local/bin
 
 if ! which hadolint &>/dev/null; then
-    >&2 echo 'hadolint command not found'
+    >&2 echo 'Hadolint command not found, trying to install...'
     if [ -z "$(command -v brew)" ]; then
         if [ -z "$(command -v curl)"]; then
             >&2 echo "Can't install hadolint in this machine, proceding to exit"
             exit 1
         else
-            curl -o /usr/local/bin https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Darwin-x86_64
+            >&2 echo "Installing using curl"
+            >&2 curl -o /usr/local/bin https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Darwin-x86_64
+            chmod -x /usr/local/bin/hadolint
         fi
     else
+        >&2 echo "Installing using brew"
         brew install hadolint
     fi
     
     
 fi
-
-hadolint "$@"
+>&2 echo "Executing hadolint"
+>&2 hadolint "$@"
