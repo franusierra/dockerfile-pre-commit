7204877849aedf285b8f1829194457cd865de460
franusierra <franusierra@gmail.com>
2020-03-11 13:53:41 +0100

    hotfix: change the order in the ubuntu install

--- lint.sh
+++ lint.sh
@@ -10,10 +10,10 @@ if ! which hadolint &>/dev/null; then
     >&2 echo 'hadolint command not found'
     if [ -z "$(command -v brew)" ]; then
         if [ -z "$(command -v curl)"]; then
-            curl -o /usr/local/bin https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Darwin-x86_64
-        else
             >&2 echo "Can't install hadolint in this machine, proceding to exit"
             exit 1
+        else
+            curl -o /usr/local/bin https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Darwin-x86_64
         fi
     else
         brew install hadolint
