260612ac03310c3d08c9d3dce4c5472a54f5bac2
franusierra <franusierra@gmail.com>
2020-03-12 10:34:20 +0100

    feat: add a variable to store args before curl

--- lint.sh
+++ lint.sh
@@ -5,18 +5,21 @@ set -o pipefail
 set -o nounset
 
 export PATH=$PATH:/usr/local/bin
-
+args= "$@"
+echo "Executing docker linter version 1.8"
 if ! which hadolint &>/dev/null; then
-    >&2 echo 'Hadolint command not found, trying to install...'
+    echo 'Hadolint command not found, trying to install...'
     if [ -z "$(command -v brew)" ]; then
         if [ -z "$(command -v curl)" ]; then
-            >&2 echo "Can't install hadolint in this machine, proceding to exit"
+            echo "Can't install hadolint in this machine, proceding to exit"
             exit 1
         else
-            >&2 echo "Installing using curl"
+            echo "Installing using curl"
             curl -o ~/hadolint https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Darwin-x86_64
+            echo "Hadolint instaled"
             chmod +x ~/hadolint
-            ~/hadolint "$@"
+            echo "Running hadolint with args: $args"
+            ~/hadolint $args
             exit 0
         fi
     else
@@ -26,4 +29,4 @@ if ! which hadolint &>/dev/null; then
 fi
 
  >&2 echo "Executing hadolint"
- hadolint "$@"
+ hadolint $args
